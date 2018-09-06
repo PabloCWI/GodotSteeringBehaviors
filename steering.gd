5029e8a08657c67e903b6e0a80dfa988b7e353b2
Unknown <pablo.garcia@cwi.com.br>
2018-09-05 23:55:09 -0300

    Arriving behavior

--- steering.gd
+++ steering.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 3000 #Define a velocidade Máxima
+const MAX_SPEED = 300 #Define a velocidade Máxima
 const MAX_FORCE = 0.02 #Define a força Máxima usada para 'dirigir' o vetor de velocidade.
+const arriving_radius = 1000
 var velocity = Vector2()
 onready var target = get_pos()
 
@@ -15,14 +16,21 @@ func _fixed_process(delta):
 	move(velocity * delta)
 	target = get_viewport().get_mouse_pos()
 
-func steer(target):
-	var desired_velocity = Vector2(target - get_pos()).normalized() * MAX_SPEED
+func steer(target_pos):
+	var desired_velocity = target_pos - get_pos()
+	var distance = desired_velocity.length()
 	
 	if mode == 0:
 		pass
 	if mode == 1:
 		desired_velocity = -desired_velocity
 	
+	if distance < arriving_radius:
+		desired_velocity = desired_velocity.normalized() * MAX_SPEED * (distance / arriving_radius)
+	else:
+		desired_velocity = desired_velocity.normalized() * MAX_SPEED
 	var steer = desired_velocity - velocity
 	var target_velocity = velocity + (steer * MAX_FORCE)
 	return(target_velocity)
+	
+	
